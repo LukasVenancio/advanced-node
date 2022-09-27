0589a0774c09a208fe91071b6f612503c6970d37
Lukas Santos Venancio <88518524+LukasVenancio@users.noreply.github.com>
2022-09-27 12:12:28 -0300

    Cleaning up the Project

--- src/main/api/index.ts
+++
@@ -1,7 +0,0 @@
-import '../config/module-alias'
-import { PersonController } from "@/application/controllers"
-
-const person = new PersonController()
-
-console.log(person.speak('Lukas'))
-console.log(person.speak())
