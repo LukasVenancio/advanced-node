c7f730d9775fac671b6ee2fb0e7f3aff1c1dccb4
Lukas Santos Venancio <88518524+LukasVenancio@users.noreply.github.com>
2022-10-08 09:28:13 -0300

    Simplfy code using Intersecton Types

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -5,17 +5,16 @@ import { FacebookAuthentication } from '@/domain/features'
 
 export class FacebookAuthenticationService {
   constructor (
-    private readonly LoadFacebookUserApi: LoadFacebookUserApi,
-    private readonly LoadUserAccountRepo: LoadUserAccountRepository,
-    private readonly CreateFacebookAccountRepo: CreateFacebookAccountRepository
+    private readonly facebookApi: LoadFacebookUserApi,
+    private readonly userAccountRepo: LoadUserAccountRepository & CreateFacebookAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
-    const fbData = await this.LoadFacebookUserApi.loadUser(params)
+    const fbData = await this.facebookApi.loadUser(params)
 
     if (fbData !== undefined) {
-      await this.LoadUserAccountRepo.load({ email: fbData?.email })
-      await this.CreateFacebookAccountRepo.createFacebookAccount(fbData)
+      await this.userAccountRepo.load({ email: fbData?.email })
+      await this.userAccountRepo.createFacebookAccount(fbData)
     }
 
     return new AuthenticationError()
